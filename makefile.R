8110e250ef7edb57397738015cfca5edc30bb989
gustavo-diaz <gustavodiaz@tulane.edu>
2022-08-09 13:12:40 -0400

    Adding a makefile for pdf rendering

---
+++ makefile.R
@@ -0,0 +1,17 @@
+# Render files in subfolders manually because quarto seems to ignore them
+# Run one at a time or the whole file
+
+# cv
+rmarkdown::render("files/diaz_cv.Rmd")
+
+# cover letters
+rmarkdown::render("job_market/cover_letter/cover_letter_cp.Rmd")
+rmarkdown::render("job_market/cover_letter/cover_letter_methods.Rmd")
+
+# statements
+rmarkdown::render("job_market/statements/research_statement_cp.Rmd")
+rmarkdown::render("job_market/statements/research_statement_methods.Rmd")
+rmarkdown::render("job_market/statements/teaching_statement_cp.Rmd")
+rmarkdown::render("job_market/statements/teaching_statement_methods.Rmd")
+rmarkdown::render("job_market/statements/diversity_statement.Rmd")
+rmarkdown::render("job_market/statements/diaz_teaching_evals.Rmd")
