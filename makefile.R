bed577611965b2e5b3d60bb52571587181a627ba
gustavo-diaz <gustavodiaz@tulane.edu>
2022-11-22 11:17:46 -0500

    Uploading Winter 2023 syllabus

--- makefile.R
+++ makefile.R
@@ -30,11 +30,12 @@ rmarkdown::render("job_market/statements/diaz_teaching_evals.Rmd")
 
 ## JOB SPECIFIC MATERIALS
 # Here if needed
-rmarkdown::render("job_market/cover_letter/cover_letter_ucsd.Rmd",
+rmarkdown::render("job_market/cover_letter/cover_letter_kaist.Rmd",
                   output_file = "diaz_cover_letter.pdf")
-rmarkdown::render("job_market/statements/teaching_statement_ucsd.Rmd",
+rmarkdown::render("job_market/statements/teaching_statement_kaist.Rmd",
                   output_file = "diaz_teaching_statement.pdf")
-rmarkdown::render("job_market/statements/research_statement_ucsd.Rmd",
+rmarkdown::render("job_market/statements/research_statement_kaist.Rmd",
                   output_file = "diaz_research_statement.pdf")
+rmarkdown::render("files/diaz_cv_kaist.Rmd")
 
 
