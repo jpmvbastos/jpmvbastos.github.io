dd513bee862d4de5b6aea14ed5815fefc551e4bb
gustavo-diaz <gustavodiaz@tulane.edu>
2023-01-18 16:17:44 -0500

    updates to job specific cover letter

--- makefile.R
+++ makefile.R
@@ -30,7 +30,7 @@ rmarkdown::render("job_market/statements/diaz_teaching_evals.Rmd")
 
 ## JOB SPECIFIC MATERIALS
 # Here if needed
-rmarkdown::render("job_market/cover_letter/cover_letter_kaist.Rmd",
+rmarkdown::render("job_market/cover_letter/cover_letter_ucsd.Rmd",
                   output_file = "diaz_cover_letter.pdf")
 rmarkdown::render("job_market/statements/teaching_statement_kaist.Rmd",
                   output_file = "diaz_teaching_statement.pdf")
