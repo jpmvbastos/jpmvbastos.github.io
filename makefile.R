43432781558904bcf72e379d1227686d303a0cc8
gustavo-diaz <diazg2@mcmaster.ca>
2023-07-27 16:22:48 -0400

    20230727 methods diversity statement

--- makefile.R
+++ makefile.R
@@ -8,7 +8,7 @@ rmarkdown::render("files/diaz_cv.Rmd")
 # methods
 quarto::quarto_render("job_market/cover_letter/cover_letter_methods.qmd", 
                       output_file = "diaz_cover_letter.pdf")
-rmarkdown::render("job_market/statements/research_statement_methods.Rmd",
+quarto::quarto_render("job_market/statements/research_statement_methods.qmd",
                   output_file = "diaz_research_statement.pdf")
 rmarkdown::render("job_market/statements/teaching_statement_methods.Rmd",
                   output_file = "diaz_teaching_statement.pdf")
