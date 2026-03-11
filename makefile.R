a86ebbf16846fdd8e1b963409ee2bfd2f084d0a8
João Pedro Bastos <74678309+jpmvbastos@users.noreply.github.com>
2026-03-10 23:47:32 -0500

    update

--- makefile.R
+++
@@ -1,6 +0,0 @@
-# Render files in subfolders manually because quarto seems to ignore them
-# Run one at a time depending on the application
-
-# cv
-rmarkdown::render("files/cv.Rmd") # For website
-rmarkdown::render("files/diaz_cv.Rmd") # For job apps
